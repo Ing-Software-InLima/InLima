a9ec9e794af1246f8bd59e7398e854396a4bf7ab
Fabricio <casta2804@gmail.com>
2024-05-07 01:35:26 -0600

    gestion_pipipi

--- src/app/gestion/page.jsx
+++ src/app/gestion/page.jsx
@@ -1,108 +1,91 @@
+"use client";
 import './css/gestion.css';
-{/*
-import { useClient } from 'next/dist/shared/lib/router-context'; // Importa useClient
-import { useSearchParams } from 'next/navigation'; // Cambia la importación a next/navigation
 import { useState } from 'react';
-import { useRouter } from 'next/router';*/ }
+import { useRouter } from 'next/navigation';
 
 export default function GestionPage() {
-    {/*
-    useClient(); // Marca el componente padre como Client Component
 
-    const searchParams = useSearchParams();
-    const router = useRouter()
+    const [asunto, setAsunto] = useState('');
+    const [ubicacion, setUbicacion] = useState('');
+    const router = useRouter();
 
-    // Parametros de busqueda
-    const [AsuntoQueja, setAsuntoQueja] = useState("");
-    const [Ubicacion, setUbicacion] = useState("");
+    const handleLimpiar = () => {
+        // Limpiar los valores de los campos de entrada
+        setAsunto('');
+        setUbicacion('');
+    };
 
-
-    const handleSearch = async (e) => {
-        // escribe en la URL visualmente
-        const params = new URLSearchParams(searchParams);
-        params.set('asunto', AsuntoQueja);
-        params.set('ubicacion', Ubicacion);
-        console.log(params.toString())
-        //router.push(`/resultados?${params.toString()}`)
-    };*/ }
+    const handleSearch = (e) => {
+        // Construye la URL de búsqueda con los parámetros de asunto y ubicación
+        const queryParams = new URLSearchParams({ asunto, ubicacion }).toString();
+        // Redirige a la página de resultados con los parámetros de búsqueda
+        router.push(`/resultados?${queryParams}`);
+    };
 
     return (
         <>
             <div id="form">
-                    <div className="border-b border-gray-300" id="titulo">
-                        <p className="pb-2"> Busqueda</p>
-                    </div>
-                    <div className=" py-4 px-4 ">
-                        <form className="flex" //onSubmit={(e) => { e.preventDefault() }}
-                        >
-                            <div className="w-1/2 mr-4 space-y-4 m-3 ">
+                <div className="border-b border-gray-300" id="titulo">
+                    <p className="pb-2"> Busqueda</p>
+                </div>
+                <div className=" py-4 px-4 ">
+                    <form className="flex"  onSubmit={(e)=>{e.preventDefault()}}>
+                        <div className="mr-4 space-y-4 m-3 ">
 
-                                <div id="text_field">
-                                    <div class="borde_text_field">
-                                        <div class="state_layer">
-                                            <div class="content">
-                                                <div id="text_type">
-                                                    <p>Ingresa el asunto de la queja</p>
-                                                </div>
-                                                <div id="input_text">
-                                                    <input type='text' placeholder='' id="inputAsunto" //value={AsuntoQueja} onChange={(e) => setAsuntoQueja(e.target.value)} 
-                                                    />
-                                                </div>
+                            <div id="text_field">
+                                <div class="borde_text_field">
+                                    <div class="state_layer">
+                                        <div class="content">
+                                            <div id="text_type">
+                                                <p>Ingresa el asunto de la queja</p>
+                                            </div>
+                                            <div id="input_text">
+                                                <input type='text' placeholder='' id="input_text" value={asunto} onChange={(e) => setAsunto(e.target.value)} />
                                             </div>
                                         </div>
                                     </div>
-
-                                    <div class="supporting-text">
-                                        <p></p>
-                                    </div>
-
                                 </div>
 
-                                <div id="text_field">
-                                    <div class="borde_text_field">
-                                        <div class="state_layer">
-                                            <div class="content">
-                                                <div id="text_type">
-                                                    <p>Ingrese Ubicación</p>
-                                                </div>
-                                                <div id="input_text">
-                                                    <input type='text' placeholder='' id="inputUbi" //value={Ubicacion} onChange={(e) => setUbicacion(e.target.value)} 
-                                                    />
-                                                </div>
+                                <div class="supporting-text">
+                                    <p></p>
+                                </div>
+
+                            </div>
+
+                            <div id="text_field">
+                                <div class="borde_text_field">
+                                    <div class="state_layer">
+                                        <div class="content">
+                                            <div id="text_type">
+                                                <p>Ingrese Ubicación</p>
+                                            </div>
+                                            <div id="input_text">
+                                                <input type='text' placeholder='' id="input_text" value={ubicacion} onChange={(e) => setUbicacion(e.target.value)} />
                                             </div>
                                         </div>
                                     </div>
+                                </div>
 
-                                    <div class="supporting-text">
-                                        <p></p>
-                                    </div>
+                                <div class="supporting-text">
+                                    <p></p>
+                                </div>
 
                                 </div>
 
-                            </div>
-
-
-                            <div className="w-1/2">
-                                <div className="mb-4">
-
-                                    <div className="space-y-2">
-
-                                    </div>
+                                <div className="text-left space-x-2 ">
+                                    <button type="reset" onClick={handleLimpiar} className="px-4 py-2 hover:text-inLima_red border rounded-full py-1">Limpiar</button>
+                                    <button type="submit" onClick={handleSearch} className="bg-inLima_beige px-4 py-2 hover:bg-inLima_red hover:text-white border rounded-full text-inLima_red  py-1">Buscar</button>
                                 </div>
-                            </div>
-                            <div className="text-right space-x-2 ">
-                                <button type="reset" className="px-4 py-2 hover:text-inLima_red border rounded-full py-1">Limpiar</button>
-                                <button type="submit" //onClick={handleSearch} 
-                                    className="bg-inLima_beige px-4 py-2 hover:bg-inLima_red border rounded-full text-white py-1" >Buscar</button>
-                            </div>
 
+                            </div>
+                                
                         </form>
 
                     </div>
 
-            </div>
+                </div>
 
-        </>
+            </>
 
-    )
-}
+        )
+    }
---
+++ src/app/resultados/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function ResultadosPage() {
+
+
+    return (
+        <>
+            <div className="border-b border-gray-300" id="titulo">
+                <p className="pb-2"> Busqueda - Resultados</p>
+            </div>
+
+        </>
+
+    )
+}
