cc4f425edad243de273ff21ec424cef36ab740a6
mateo <mateo27965@gmail.com>
2024-05-09 00:16:11 -0500

    aaa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     return (
         <nav className="flex-none bg-inLima_beige px-8 py-9">
             <ul className="flex flex-col gap-y-5 text-inLima_red font-semibold pr-8 pl-2">
-                <li><Link href="/menu">Quejas</Link></li>
+                <li><Link href="/home">Quejas</Link></li>
                 <li><Link href="/estado">Estado</Link></li>
                 <li><Link href="/perfil">Perfil</Link></li>
             </ul>
