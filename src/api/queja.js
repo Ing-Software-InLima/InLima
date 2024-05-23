fd196e2ba3a5476da2073850ab431e62be6fd10a
Fabricio <casta2804@gmail.com>
2024-05-23 00:57:17 -0600

    busqueda

--- src/api/queja.js
+++ src/api/queja.js
@@ -8,8 +8,17 @@ const findAll = async () => await base.get(endpoint);
 
 const create = async (payload) => await base.post(endpoint, payload);
 
+const obtenerQuejasFiltradas = async (asuntos, municipalidad) => {
+    return await base.get('/queja/search', {
+        params: {
+            asuntos: asuntos.join(','),
+            municipalidad,
+        },
+    });
+};
+
 //const verQueja = async()
 
-const api = { findAll, create, quejasUsuario }
+const api = { findAll, create, quejasUsuario , obtenerQuejasFiltradas }
 
 export default api;
--- src/app/resultados/page.jsx
+++ src/app/resultados/page.jsx
@@ -3,60 +3,43 @@ import React, { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import StatusCard from "@/components/StatusCard";
 import Layout from '@/components/Layout';
+import api from '@/api/queja'
 
 export default function ResultadosPage() {
-    // Ejemplo de quejas
-    const quejas = [
-        {
-            id: 1,
-            asunto: "Veredas rotas",
-            dni: "12345678",
-            estado: "Enviado",
-            municipalidad: "Ate"
-        },
-        {
-            id: 2,
-            asunto: "Calles contaminadas",
-            dni: "87654321",
-            estado: "En proceso",
-            municipalidad: "San Isidro"
-        },
-    ];
-
+    
     const router = useRouter();
-    const searchParams = useSearchParams(); // Uso de useSearchParams correctamente
+    const searchParams = useSearchParams();
     const [resultados, setResultados] = useState([]);
 
     useEffect(() => {
         if (!router.isReady) return;
-        const params = new URLSearchParams(searchParams.toString()); // Convertir a URLSearchParams
-        const asuntos = params.get('asuntos') ? params.get('asuntos').split(',') : [];
-        const municipalidad = params.get('municipalidad') || '';
-        
-        console.log("Asuntos recibidos:", asuntos); // Impresión de consola para verificar asuntos
-        console.log("Municipalidad recibida:", municipalidad); // Impresión de consola para verificar municipalidad
-        
-        realizarBusqueda(asuntos, municipalidad);
+
+        const fetchQuejas = async () => {
+            const params = new URLSearchParams(searchParams.toString());
+            const asuntos = params.get('asuntos') ? params.get('asuntos').split(',') : [];
+            const municipalidad = params.get('municipalidad') || '';
+
+            try {
+                const response = await api.obtenerQuejasFiltradas(asuntos, municipalidad);
+                console.log("Quejas recibidas:", response.data); // Impresión de consola para verificar los datos
+                setResultados(response.data);
+            } catch (error) {
+                console.error('Error al obtener las quejas:', error);
+            }
+        };
+
+        fetchQuejas();
     }, [router.isReady, searchParams]);
 
-    const realizarBusqueda = (asuntosSeleccionados, municipalidad) => {
-        console.log("Realizando búsqueda con:", {asuntosSeleccionados, municipalidad}); // Impresión de consola antes de la búsqueda
-        const resultadosFiltrados = quejas.filter(
-            (queja) =>
-                (asuntosSeleccionados.length === 0 ||
-                    asuntosSeleccionados.some(asunto => 
-                        asunto.toLowerCase() === queja.asunto.toLowerCase())) &&
-                (!municipalidad || queja.municipalidad.toLowerCase() === municipalidad.toLowerCase())
-        );
-        setResultados(resultadosFiltrados);
-        console.log("Resultados filtrados:", resultadosFiltrados); // Impresión de consola para verificar los resultados filtrados
-    };
 
     return (
         <Layout>
             <div>
                 <div className="border-b border-gray-300" id="titulo">
                     <p className="pb-2">Resultados</p>
+                    <div className="text-left space-x-2 ">
+                        <button type="submit" className="bg-inLima_beige hover:bg-inLima_red hover:text-white border rounded-full text-inLima_red  py-1">Volver a buscar</button>
+                    </div>
                 </div>
                 {resultados.length === 0 ? (
                     <p>No se encontraron resultados para esta búsqueda</p>
