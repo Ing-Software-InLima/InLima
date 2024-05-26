e5473a9235bc15f315f37ca612b25976ba633351
RLJ17 <20201192@aloe.ulima.edu.pe>
2024-05-26 18:35:27 -0500

    Logout fixed

--- src/components/HeaderInLima.jsx
+++ src/components/HeaderInLima.jsx
@@ -1,9 +1,7 @@
-'use client'
 import api_usuario from "@/api/usuario"
 import { useRouter } from "next/navigation"
 
 export default function HeaderInLima({ toggleNav, Nav }) {
-    const router = useRouter()
     return (
         <div className="bg-inLima_red py-5 text-center text-white font-bold text-xl flex justify-between">
             <button onClick={toggleNav} className={`ml-5 ${Nav ? 'rotate-0' : 'rotate-90'} hover:text-gray-200`}>
@@ -15,7 +13,7 @@ export default function HeaderInLima({ toggleNav, Nav }) {
             <div className="mr-5">
                 <button onClick={() => {
                     api_usuario.cerrarSesion()
-                    router.push('/login')
+                    window.location.href = '/login';
                 }}>Exit</button>
             </div>
         </div>
--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
+
 export async function middleware(request) {
     const pass = new TextEncoder().encode("secret") //formato que pide jose
     const token = request.cookies.get('myToken')
@@ -7,15 +8,15 @@ export async function middleware(request) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
     try {
-        console.log(token)
-        await jwtVerify(token?.value, pass)
-        return NextResponse.next()
+        
+        await jwtVerify(token?.value, pass);
+        return NextResponse.next();
+
     } catch (error) {
         console.error(error)
         console.log("No token")
         return NextResponse.redirect(new URL('/login', request.url))
     }
-    return NextResponse.next()
 }
 
 export const config = {
